2cc5cad56df9357622422a558520a9556ffdffe3
Yipeng Sun <syp@umd.edu>
2022-09-21 17:30:05 -0400

    Also compute UBDT veto.

---
+++ efficiency_gen/rdx-run2-ubdt_veto.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Note: Run this on glacier!
+
+declare -A SAMPLES
+SAMPLES[Mu_nopt]="DLLmu > 2.0 & DLLe < 1.0 & IsMuon == 1.0 & UBDT <= 0.25"
+
+for year in 16; do
+    for polarity in "up" "down"; do
+        for part in "${!SAMPLES[@]}"; do
+            pidcalib2.make_eff_hists \
+                --output-dir pidcalib_output \
+                --sample "Turbo${year}" --magnet ${polarity} \
+                --particle ${part} --pid-cut "${SAMPLES[${part}]}" \
+                --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel
+        done
+    done
+done
