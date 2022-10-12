e8a40f794d4d82582607137ad7d6379d76674015
Yipeng Sun <syp@umd.edu>
2022-10-12 19:00:15 -0400

    Use a custom binning.

--- efficiency_gen/rdx-run2-ubdt_veto.sh
+++ efficiency_gen/rdx-run2-ubdt_veto.sh
@@ -12,7 +12,8 @@ for year in 16; do
                 --output-dir pidcalib_output \
                 --sample "Turbo${year}" --magnet ${polarity} \
                 --particle ${part} --pid-cut "${SAMPLES[${part}]}" \
-                --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel
+                --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel \
+                --binning-file ./binning.json
         done
     done
 done
