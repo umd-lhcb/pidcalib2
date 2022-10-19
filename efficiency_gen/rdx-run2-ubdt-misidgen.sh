a4c08c1e996047af31037543edb8001d17f1bec5
Yipeng Sun <syp@umd.edu>
2022-10-19 08:13:52 -0400

    Generate the correct efficiencies.

--- efficiency_gen/rdx-run2-ubdt-misidgen.sh
+++ efficiency_gen/rdx-run2-ubdt-misidgen.sh
@@ -3,15 +3,23 @@
 # Note: Run this on glacier!
 # Generates files necessary for efficiency vs. bkg efficiency plots
 
-rm -rf pidcalib_ubdt_eff
+OUTPUT_FOLDER=pidcalib_ubdt_eff
+rm -rf ${OUTPUT_FOLDER}
+
+declare -A PREFIX
+PREFIX[K]="Turbo"
+PREFIX[Pi]="Turbo"
+PREFIX[P]="Turbo"
+PREFIX[Mu_nopt]="Turbo"
+PREFIX[e_B_Jpsi]="Electron"
 
 for year in 16; do
     for polarity in "up"; do
         for var in "Brunel_P" "Brunel_PT"; do
-            for part in "Mu" "K" "Pi" "P" "e_B_Jpsi"; do
+            for part in "Mu_nopt" "K" "Pi" "P"; do
                 pidcalib2.make_eff_hists \
-                    --output-dir pidcalib_ubdt_eff \
-                    --sample "Turbo${year}" --magnet ${polarity} \
+                    --output-dir ${OUTPUT_FOLDER} \
+                    --sample "${PREFIX[${part}]}${year}" --magnet ${polarity} \
                     --particle ${part} \
                     $(for I in $(seq 0 0.05 0.3); do echo "--pid-cut UBDT>${I} "; done) \
                     $(for I in $(seq 0 0.05 0.3); do echo "--pid-cut Brunel_MC15TuneV1_ProbNNmu>${I} "; done) \
