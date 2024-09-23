8ee9b18129eec182d1215e1bf3f175751961601e
Alex Fernez <afernez@umd.edu>
2024-09-23 16:58:09 -0400

    commit so that friend tuples w ubdt use xml without DLLMu cut

--- src/pidcalib2/pid_data.py
+++ src/pidcalib2/pid_data.py
@@ -248,7 +248,7 @@ def root_to_dataframe(
         root_file = uproot.open(path)
 
         # NOTE: UMD special
-        friend_file = uproot.open(path.replace('remote', 'friends'))
+        friend_file = uproot.open(path.replace('remote', 'friends-no_cut'))
     except FileNotFoundError as exc:
         if "Server responded with an error: [3010]" in exc.args[0]:
             log.error(
